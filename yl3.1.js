39b70ea73c92205bd4d076c77db9e2968f08c428
Jakob Kaur <Jakob-kaur.kelder@voco.ee>
2024-09-05 11:47:39 +0300

    yl3.1.js

--- yl3.1.js
+++ yl3.1.js
@@ -1,13 +1,20 @@
-const readline = require('readline').createInterface({
+const readline = require('readline');
+
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
-  
-  readline.question('Sisesta astme alus (täisarv): ', (alus) => {
-    readline.question('Sisesta astendaja (täisarv): ', (astendaja) => {
-      const result = alus ** astendaja;
-      console.log(vastus);
-      readline.close();
-    });
-  });
-  
+});
+
+rl.question('Sisesta, mitu korda äratus heliseb: ', (count) => {
+    count = parseInt(count, 10); 
+
+    if (isNaN(count) || count <= 0) {
+        console.log("Palun sisesta positiivne täisarv.");
+    } else {
+        for (let i = 0; i < count; i++) {
+            console.log('Tõuse ja sära!');
+        }
+    }
+
+    rl.close();
+});
